848c6424dd79a476ed1ced1fd7c01cdbd18f4c1f
shihao <shihao_kiss@outlook.com>
2018-07-22 18:59:45 +0800

    获取文件中单词个数函数优化.

--- week3/week2.py
+++ week3/week2.py
@@ -1,4 +1,6 @@
 # -*- coding:utf-8 -*-
+import commands
+
 
 def findinlist(targetlist, x):
     return x in targetlist
@@ -6,6 +8,15 @@ def findinlist(targetlist, x):
 def wordcount(file, word):
     return sum([line.count(word) for line in open(file, 'r')])
 
+def wordcount2(file, word):
+    command = "grep -wo %s %s | wc -l" % (word, file)
+    status, output = commands.getstatusoutput(command)
+    if status != 0:
+        return 0
+    else:
+        return int(output)
+
 if __name__ == '__main__':
     # print(findinlist(['a', 'b'], 'b'))
-    print(wordcount('words.txt', 'is'))
+    # print(wordcount('words.txt', 'is'))
+    print(wordcount2('t2', 'world'))
